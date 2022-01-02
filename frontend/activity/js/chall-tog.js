a00a7bf444efa6c388b15074f26eaf477393058f
sanghee01 <sang6299@naver.com>
2022-01-02 23:07:43 +0900

    [feat] 다중토글 완성. 근데 아이콘이 안변함

--- frontend/activity/js/chall-tog.js
+++ frontend/activity/js/chall-tog.js
@@ -1,15 +1,30 @@
-const ctoggle = document.querySelector(".challenge__comment-tog");
-const ctogCont = document.querySelector(".challenge__comment");
-const cicon = document.querySelector(".fa-chevron-up");
+const ctoggle = document.querySelectorAll(".challenge__comment-tog");
+const cicon = document.querySelectorAll(".fa-chevron-up");
 
-ctoggle.addEventListener("click", function () {
-  if (ctogCont.style.display === "block") {
-    ctogCont.style.display = "none";
-    cicon.classList.remove("fa-chevron-down");
-    cicon.classList.add("fa-chevron-up");
-  } else {
-    ctogCont.style.display = "block";
-    cicon.classList.remove("fa-chevron-up");
-    cicon.classList.add("fa-chevron-down");
-  }
-});
+ctoggle.forEach((toggle) =>
+  toggle.addEventListener("click", function () {
+    const togId = this.id.replace("tog", "comm");
+
+    if (document.getElementById(togId).style.display === "block") {
+      document.getElementById(togId).style.display = "none";
+      // console.log(this.children.value);
+      // console.dir(this);
+      // this.children.classList.remove("fa-chevron-down");
+      // this.children.classList.remove("fa-chevron-up");
+    } else {
+      document.getElementById(togId).style.display = "block";
+    }
+  })
+);
+
+// ctoggle.addEventListener("click", function () {
+//   if (ctogCont.style.display === "block") {
+//     ctogCont.style.display = "none";
+//     cicon.classList.remove("fa-chevron-down");
+//     cicon.classList.add("fa-chevron-up");
+//   } else {
+//     ctogCont.style.display = "block";
+//     cicon.classList.remove("fa-chevron-up");
+//     cicon.classList.add("fa-chevron-down");
+//   }
+// });
