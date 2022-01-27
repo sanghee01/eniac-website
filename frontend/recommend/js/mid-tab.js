c73cfae96a6114771596ff600fadf90f51da5996
sanghee01 <sang6299@naver.com>
2022-01-27 21:58:37 +0900

    [feat] 내부 탭 기능 수정 북마크3

--- frontend/recommend/js/mid-tab.js
+++ frontend/recommend/js/mid-tab.js
@@ -1,12 +1,23 @@
-const midTabList = document.querySelectorAll(".mid-tab-menu__list");
-const midTabBtn = document.querySelectorAll(".mid-tab-menu__btn");
+const midTabList_1 = document.querySelectorAll(".mlist-1");
+const midTabList_2 = document.querySelectorAll(".mlist-2");
+const midTabBtn_1 = document.querySelectorAll(".mbtn-1");
+const midTabBtn_2 = document.querySelectorAll(".mbtn-2");
 
-for (let i = 0; i < midTabList.length; i++) {
-  midTabBtn[i].addEventListener("click", function (e) {
+for (let i = 0; i < midTabList_1.length; i++) {
+  midTabBtn_1[i].addEventListener("click", function (e) {
     e.preventDefault();
-    for (let j = 0; j < midTabList.length; j++) {
-      console.dir(midTabList);
-      midTabList[j].classList.remove("is_on");
+    for (let j = 0; j < midTabList_1.length; j++) {
+      midTabList_1[j].classList.remove("is_on");
+    }
+    this.parentNode.classList.add("is_on");
+  });
+}
+
+for (let i = 0; i < midTabList_2.length; i++) {
+  midTabBtn_2[i].addEventListener("click", function (e) {
+    e.preventDefault();
+    for (let j = 0; j < midTabList_2.length; j++) {
+      midTabList_2[j].classList.remove("is_on");
     }
     this.parentNode.classList.add("is_on");
   });
