59bed61b3126981373a798f1af8310c051b00b6a
dongyu-youn <donghe1472@naver.com>
2022-02-06 18:24:01 +0900

    [Feat] login merge중

--- users/models.py
+++ users/models.py
@@ -6,6 +6,10 @@ from django.shortcuts import reverse
 import uuid
 from django.conf import settings
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from config import settings
+
 
 
 # Create your models here.
@@ -41,11 +45,16 @@ class User(AbstractUser):
         if self.email_verified is False:
             secret = uuid.uuid4().hex[:20]
             self.email_secret = secret
+            html_message = render_to_string(
+                "email/verify_email.html", {"secret": secret}
+            )
             send_mail(
-                "Verify Airbnb Account",
-                f"Verify account, this is your secret: {secret}",
+                "Verify Hairbnb Account",
+                strip_tags(html_message),
                 settings.EMAIL_FROM,
                 [self.email],
-                fail_silently=False,
+                html_message=html_message,
             )
+
+            self.save()
         return
